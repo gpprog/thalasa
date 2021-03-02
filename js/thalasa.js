b7bd6130180b82e370feeda37dbd353c91a50b8e
unknown <gpapoutzas@gmail.com>
2021-03-02 20:04:54 +0200

    First Site View

---
+++ js/thalasa.js
@@ -0,0 +1,9 @@
+
+
+
+var submit = document.getElementById("submit");
+
+submit.onclick = function() {
+    let name = document.getElementById('name').value;
+    window.alert(" Hello"+ " " +name +" "+"how are you today?");
+} 
